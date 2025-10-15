90b898b8fb513d9ad23b9cfb2f894ccb0e6cd7d2
Puk06 <86549420+puk06@users.noreply.github.com>
2025-10-16 08:39:48 +0900

    RegisterReplacedObjectを一度無効化

--- Runtime/PukoTextureReplacer.cs
+++ Runtime/PukoTextureReplacer.cs
@@ -8,15 +8,21 @@ namespace net.puk06.TextureReplacer
     public class PukoTextureReplacer : MonoBehaviour, VRC.SDKBase.IEditorOnly
     {
         [Header("スクリプトの有効 / 無効")]
+        [Tooltip("スクリプト本体の有効 / 無効を切り替えます")]
         public bool Enabled = true;
 
         [Header("プレビューの有効 / 無効")]
+        [Tooltip("NDMFのリアルタイムプレビューの有効 / 無効を切り替えます")]
         public bool PreviewEnabled = true;
 
+        [Space(10)]
+
         [Header("置き換え元のテクスチャ")]
+        [Tooltip("アバター内のこのテクスチャ全てが置き換えられます")]
         public Texture2D? sourceTexture = null;
 
         [Header("置き換え後のテクスチャ")]
+        [Tooltip("指定したテクスチャがこのテクスチャに全て置き換えられます")]
         public Texture2D? destinationTexture = null;
     }
 }
