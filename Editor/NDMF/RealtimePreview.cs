9be77905d6cbced4709b100a47c6e431c76cef4a
Puk06 <86549420+puk06@users.noreply.github.com>
2026-03-01 23:59:20 +0900

    fix: prevent duplicate avatar recognition in GetTargetGroups

--- Editor/NDMF/RealtimePreview.cs
+++ Editor/NDMF/RealtimePreview.cs
@@ -17,7 +17,7 @@ namespace net.puk06.TextureReplacer.Editor.Ndmf
     {
         public ImmutableList<RenderGroup> GetTargetGroups(ComputeContext context)
         {
-            ImmutableList<GameObject> avatarGameObjects = context.GetAvatarRoots();
+            IEnumerable<GameObject> avatarGameObjects = context.GetAvatarRoots().Distinct();
 
             List<RenderGroup> targetRenderGroups = new();
 
